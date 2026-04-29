2910d2ee7521de308068169948fcb1b8509a12ff
Rishi <rishimaheshwari040@gmail.com>
2026-04-29 16:24:14 +0530

    up

--- src/config/api.js
+++ src/config/api.js
@@ -1,5 +1,6 @@
 // API Configuration
-const BASE_URL = 
-   'http://localhost:8080/api/v1';
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL 
+  ? `${process.env.NEXT_PUBLIC_API_URL}/api/v1`
+  : 'http://localhost:8080/api/v1';
 
 export { BASE_URL };
