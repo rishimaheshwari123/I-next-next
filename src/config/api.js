8bfc8fb90dca81ef5da26bf3ea526e500f9e103c
Rishi <rishimaheshwari040@gmail.com>
2026-05-02 10:02:27 +0530

    fix

--- src/config/api.js
+++ src/config/api.js
@@ -1,5 +1,5 @@
 // API Configuration
-const BASE_URL = 'http://localhost:8080/api/v1';
-// const BASE_URL = 'https://i-next-next.onrender.com/api/v1';
+// const BASE_URL = 'http://localhost:8080/api/v1';
+const BASE_URL = 'https://i-next-next.onrender.com/api/v1';
 
 export { BASE_URL };
