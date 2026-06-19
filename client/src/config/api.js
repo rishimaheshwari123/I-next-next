12b081a6330598a10aa5c76d04dec03cff681c96
Rishi <rishimaheshwari040@gmail.com>
2026-06-19 13:32:19 +0530

    change url

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -1,6 +1,7 @@
 // API Configuration
 // export const BASE_URL = 'http://localhost:8080/api/v1';
-const BASE_URL = 'https://i-next-next.onrender.com/api/v1';
+// const BASE_URL = 'https://i-next-next.onrender.com/api/v1';
+const BASE_URL = 'https://api.inextets.in/api/v1';
 
 // Employee Management API Endpoints
 export const EMPLOYEE_API = {
