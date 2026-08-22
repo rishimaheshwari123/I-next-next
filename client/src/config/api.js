c6c643dc57fea6a924ba608b26bd3456afb18bcf
Rishi <rishimaheshwari040@gmail.com>
2026-08-22 15:49:03 +0530

    upda

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -1,5 +1,5 @@
-const BASE_URL = 'http://localhost:8085/api/v1';
-// const BASE_URL =    'https://api.inextets.in/api/v1';
+// const BASE_URL = 'http://localhost:8085/api/v1';
+const BASE_URL = 'https://api.inextets.in/api/v1';
 
 
 
